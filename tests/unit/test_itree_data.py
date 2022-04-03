d954658ea958ccf287e3fbe42346ec4ed826ac62
Gonzalo <63021395+chacabuco@users.noreply.github.com>
2022-04-03 22:12:54 +0200

    More test cases
    Added test cases for
    -_formatter
    -check
    -__contains__
    -__format__
    -__repr__

--- tests/unit/test_itree_data.py
+++ tests/unit/test_itree_data.py
@@ -69,6 +69,20 @@ class TestiTDataModelMethods:
     def test__validator(self, setup_fake_argument):
         assert setup_fake_argument._validator(None) == (True, 'ok')
 
+    def test_emptyformatter_empty(self, setup_no_argument):
+        assert setup_no_argument._formatter() is 'None'
+
+    def test_emptyformatter_not_empty(self, setup_fake_argument):
+        assert setup_fake_argument._formatter() is 'FAKE'
+
+    def test_formatter_empty(self, setup_no_argument):
+        assert setup_no_argument._formatter('Kraftfahrzeug-Haftpflichtversicherung') is \
+               'Kraftfahrzeug-Haftpflichtversicherung'
+
+    def test_formatter_empty_not_empty(self, setup_fake_argument):
+        assert setup_fake_argument._formatter('Massenkommunikationsdienstleistungsunternehmen') is \
+               'Massenkommunikationsdienstleistungsunternehmen'
+
     @pytest.mark.parametrize("state, expected", state_data)
     def test_set_state(self, state, expected):
         object_under_test = ObservableiDataModel(state=state)
@@ -78,3 +92,45 @@ class TestiTDataModelMethods:
         else:
             object_under_test.set(expected)
             assert object_under_test.value == __argument__
+
+    @pytest.mark.parametrize("state, expected", state_data)
+    def test_check(self, state, expected):
+        object_under_test = ObservableiDataModel(state=state)
+        if state is False:
+            assert object_under_test.check(3) == (False, 'ok')
+        else:
+            assert object_under_test.check(5) == (True, 'ok')
+
+    def test_contains(self, setup_fake_argument):
+       assert __argument__ in setup_fake_argument
+
+    def test_not_contains(self, setup_fake_argument):
+       assert None not in setup_fake_argument
+
+    def test_contains_empty(self, setup_no_argument):
+        # Failing, need to define what happens here.
+        # When calling None in an empty list we get False.
+       assert None in setup_no_argument
+
+    def test_not_contains_empty(self, setup_no_argument):
+       assert __argument__ not in setup_no_argument
+
+    def test_format_empty_no_format_spec(self, setup_no_argument):
+        assert format(setup_no_argument) is 'None'
+
+    def test_no_format_spec(self, setup_fake_argument):
+        assert format(setup_fake_argument) is __argument__
+
+    def test_format_spec(self, setup_fake_argument):
+        # Failing here.
+        assert format(Data.iTDataModel(10), 'x') is 'a'
+
+    def test_repr_empty(self, setup_no_argument):
+        assert str(setup_no_argument) == 'iTreeDataModel()'
+
+    def test_repr_not_empty(self, setup_fake_argument):
+        assert str(setup_fake_argument) == 'iTreeDataModel(value= %s)' %__argument__
+
+
+
+
