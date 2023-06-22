1a3fe750186c89f61f62059d309e7902c43e4383
BR1py <br_development@posteo.org>
2023-06-22 23:09:12 +0200

    Rebuild of the wheel (missing folder), delted some outdated tests

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='itertree',
     version='1.0.0',
-    packages=['itertree', 'itertree.examples'],
+    packages=['itertree', 'itertree.examples', 'itertree.itree_serializer'],
     package_dir={'': 'src'},
     url='https://github.com/BR1py/itertree',
     license='MIT',
