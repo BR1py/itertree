024dab99057dc39e2f6e69358cc649527c95626e
BR1py <br_development@posteo.org>
2023-07-01 09:40:45 +0200

    Must push again now for release 1.0.5 because of issues in PyPi

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='itertree',
-    version='1.0.4',
+    version='1.0.5',
     packages=['itertree', 'itertree.examples', 'itertree.itree_serializer'],
     package_dir={'': 'src'},
     url='https://github.com/BR1py/itertree',
